63df6318ace5a72fba42f774d083aeefab28242b
shwetha <shwethakraman57@gmail.com>
2017-10-29 12:09:00 +0530

    Basic Layout

---
+++ app/src/main/java/com/example/shwetha/blockdata/FileTransferAndLedger.java
@@ -0,0 +1,16 @@
+package com.example.shwetha.blockdata;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SHWETHA on 29-10-2017.
+ */
+public class FileTransferAndLedger extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filetransferandledger);
+    }
+}
