e4d766356a3460a8b4636421a5d42a882600a147
Kshitij13579 <kshitijyerande@gmail.com>
2017-11-02 21:54:26 +0530

    onclick for file transfer added

--- app/src/main/java/com/example/shwetha/blockdata/FileTransferAndLedger.java
+++ app/src/main/java/com/example/shwetha/blockdata/FileTransferAndLedger.java
@@ -1,16 +1,91 @@
 package com.example.shwetha.blockdata;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by SHWETHA on 29-10-2017.
  */
 public class FileTransferAndLedger extends AppCompatActivity {
+    Button filetransfer;
+    public static final String URL ="http://192.168.0.103:8081/Blockchain/BlockchainServer?data=5&req='file'";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filetransferandledger);
+        filetransfer = (Button) findViewById(R.id.allow);
+
+
+        filetransfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetXMLTask task = new GetXMLTask();
+                task.execute(new String[] { URL });
+            }
+        });
+    }
+    private class GetXMLTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String output = null;
+            for (String url : urls) {
+                output = getOutputFromUrl(url);
+            }
+            return output;
+        }
+
+        private String getOutputFromUrl(String url) {
+            StringBuffer output = new StringBuffer("");
+            try {
+                InputStream stream = getHttpConnection(url);
+               BufferedReader buffer = new BufferedReader(
+                        new InputStreamReader(stream));
+                String s = "";
+                while ((s = buffer.readLine()) != null)
+                    output.append(s);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return output.toString();
+        }
+
+        // Makes HttpURLConnection and returns InputStream
+        private InputStream getHttpConnection(String urlString)
+                throws IOException {
+            InputStream stream = null;
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+
+            try {
+                HttpURLConnection httpConnection = (HttpURLConnection) connection;
+                httpConnection.setRequestMethod("GET");
+                httpConnection.connect();
+
+                if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    stream = httpConnection.getInputStream();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return stream;
+        }
+
+        @Override
+        protected void onPostExecute(String output) {
+            Toast.makeText(getApplicationContext(),output,Toast.LENGTH_LONG).show();
+        }
     }
 }
