83399ff9a933c1caf77261c17b794d78ae24bb54
shwetha <shwethakraman57@gmail.com>
2018-01-21 22:07:11 +0530

    FileUpload

--- app/src/main/java/com/example/shwetha/blockdata/FileTransferAndLedger.java
+++ app/src/main/java/com/example/shwetha/blockdata/FileTransferAndLedger.java
@@ -1,14 +1,27 @@
 package com.example.shwetha.blockdata;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,18 +29,33 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 /**
  * Created by SHWETHA on 29-10-2017.
  */
 public class FileTransferAndLedger extends AppCompatActivity {
-    Button filetransfer;
-    public static final String URL ="http://192.168.0.103:8081/Blockchain/BlockchainServer?data=5&req='file'";
+    Button filetransfer,fileupload;
+    //public static final String URL ="http://192.168.1.103:8080/ShwethaBlockchain/BlockchainServer?data=5&req='file'";
+   public static final String URL ="http://192.168.1.103:8080/Blockchain/BlockchainServer?data=5&req='file'";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filetransferandledger);
         filetransfer = (Button) findViewById(R.id.allow);
-
+        fileupload=(Button)findViewById(R.id.uploadbutton);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
+                return;
+            }
+        }
+        enable_button();
 
         filetransfer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,9 +65,100 @@ public class FileTransferAndLedger extends AppCompatActivity {
             }
         });
     }
-    private class GetXMLTask extends AsyncTask<String, Void, String> {
+
+    private void enable_button() {
+        fileupload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialFilePicker()
+                        .withActivity(FileTransferAndLedger.this)
+                        .withRequestCode(10)
+                        .start();
+
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 100 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+            enable_button();
+        }else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
+            }
+        }
+    }
+
+    ProgressDialog progress;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+        if(requestCode == 10 && resultCode == RESULT_OK){
+
+            progress = new ProgressDialog(FileTransferAndLedger.this);
+            progress.setTitle("Uploading");
+            progress.setMessage("Please wait...");
+            progress.show();
+
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    File f  = new File(data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH));
+                    String content_type  = getMimeType(f.getPath());
+
+                    String file_path = f.getAbsolutePath();
+                    OkHttpClient client = new OkHttpClient();
+                    RequestBody file_body = RequestBody.create(MediaType.parse(content_type),f);
+
+                    RequestBody request_body = new MultipartBody.Builder()
+                            .setType(MultipartBody.FORM)
+                            .addFormDataPart("type",content_type)
+                            .addFormDataPart("uploaded_file",file_path.substring(file_path.lastIndexOf("/")+1), file_body)
+                            .build();
+
+                    Request request = new Request.Builder()
+                            .url("http://192.168.1.103:8080/Blockchain/api/upload")
+                            .post(request_body)
+                            .build();
+
+                    try {
+                        Response response = client.newCall(request).execute();
+
+                        if(!response.isSuccessful()){
+                            throw new IOException("Error : "+response);
+                        }
+
+                        progress.dismiss();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            });
+
+            t.start();
+
+
+
+
+        }
+    }
+
+    private String getMimeType(String path) {
+
+        String extension = MimeTypeMap.getFileExtensionFromUrl(path);
+
+        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+    }
+}
+
+    class GetXMLTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
+
             String output = null;
             for (String url : urls) {
                 output = getOutputFromUrl(url);
@@ -50,7 +169,9 @@ public class FileTransferAndLedger extends AppCompatActivity {
         private String getOutputFromUrl(String url) {
             StringBuffer output = new StringBuffer("");
             try {
+                Log.d("URLS",url);
                 InputStream stream = getHttpConnection(url);
+
                BufferedReader buffer = new BufferedReader(
                         new InputStreamReader(stream));
                 String s = "";
@@ -85,7 +206,7 @@ public class FileTransferAndLedger extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String output) {
-            Toast.makeText(getApplicationContext(),output,Toast.LENGTH_LONG).show();
+           // Toast.makeText(getApplica,output,   Toast.LENGTH_LONG).show();
+            System.out.println("DONE");
         }
     }
-}
