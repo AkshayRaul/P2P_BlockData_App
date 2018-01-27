55bb77fe4e25e53cf0a46c32b6abe9e07d2a60e8
shwetha <shwethakraman57@gmail.com>
2018-01-27 13:56:09 +0530

    websocket connection to server plus main intent changed from defualt

--- app/src/main/java/com/example/shwetha/blockdata/FileTransferAndLedger.java
+++ app/src/main/java/com/example/shwetha/blockdata/FileTransferAndLedger.java
@@ -1,21 +1,40 @@
 package com.example.shwetha.blockdata;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
+
+
 import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
+
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
+
 import android.webkit.MimeTypeMap;
-import android.widget.Button;
-import android.widget.Toast;
+
 
 import com.nbsp.materialfilepicker.MaterialFilePicker;
 import com.nbsp.materialfilepicker.ui.FilePickerActivity;
@@ -31,16 +50,19 @@ import java.net.URLConnection;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-/**
- * Created by SHWETHA on 29-10-2017.
- */
+///**
+// * Created by SHWETHA on 29-10-2017.
+// */
 public class FileTransferAndLedger extends AppCompatActivity {
     Button filetransfer,fileupload;
+    private Button start;
+    private TextView output;
+    private OkHttpClient client;
+    private WebSocketClient mWebSocketClient;
     //public static final String URL ="http://192.168.1.103:8080/ShwethaBlockchain/BlockchainServer?data=5&req='file'";
    public static final String URL ="http://192.168.1.103:8080/Blockchain/BlockchainServer?data=5&req='file'";
     @Override
@@ -49,6 +71,9 @@ public class FileTransferAndLedger extends AppCompatActivity {
         setContentView(R.layout.filetransferandledger);
         filetransfer = (Button) findViewById(R.id.allow);
         fileupload=(Button)findViewById(R.id.uploadbutton);
+        client = new OkHttpClient();
+        Log.i("Websocket","Client");
+        start();
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},100);
@@ -62,6 +87,54 @@ public class FileTransferAndLedger extends AppCompatActivity {
             public void onClick(View v) {
                 GetXMLTask task = new GetXMLTask();
                 task.execute(new String[] { URL });
+
+            }
+        });
+    }
+    private void start() {
+        client = new OkHttpClient.Builder()
+                .readTimeout(3,  TimeUnit.SECONDS)
+                .build();
+        Request request = new Request.Builder()
+                .url("ws://172.16.41.189:8080/Blockchain/ws")
+                .build();
+        Log.i("Websocket",request.toString());
+        WebSocket ws = client.newWebSocket(request,new WebSocketListener(){
+            public void onOpen(WebSocket webSocket, Response response) {
+                Log.d("Websocker","Connected");
+            }
+
+            /** Invoked when a text (type {@code 0x1}) message has been received. */
+            public void onMessage(WebSocket webSocket, String text) {
+            }
+
+            /** Invoked when a binary (type {@code 0x2}) message has been received. */
+            public void onMessage(WebSocket webSocket, ByteString bytes) {
+            }
+
+            /**
+             * Invoked when the remote peer has indicated that no more incoming messages will be
+             * transmitted.
+             */
+            public void onClosing(WebSocket webSocket, int code, String reason) {
+            }
+
+            /**
+             * Invoked when both peers have indicated that no more messages will be transmitted and the
+             * connection has been successfully released. No further calls to this listener will be made.
+             */
+            public void onClosed(WebSocket webSocket, int code, String reason) {
+            }
+        });
+
+        client.dispatcher().executorService().shutdown();
+    }
+
+    private void output(final String txt) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                output.setText(output.getText().toString() + "\n\n" + txt);
             }
         });
     }
@@ -209,4 +282,4 @@ public class FileTransferAndLedger extends AppCompatActivity {
            // Toast.makeText(getApplica,output,   Toast.LENGTH_LONG).show();
             System.out.println("DONE");
         }
-    }
+      }
